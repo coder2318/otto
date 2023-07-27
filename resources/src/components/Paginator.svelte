64e87c488fd119bb30c09c9297a635deb65cf999
Dmytro Morozov <dmytro.morozov@lengin.com>
2023-07-27 16:44:51 +0300

    Fix styles

--- resources/src/components/Paginator.svelte
+++ resources/src/components/Paginator.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
     import { inertia } from '@inertiajs/svelte';
-    export let meta = null;
+    export let meta : App.PaginationMeta = null;
     export let buttonClass = "";
     export let activeClass = "btn-primary";
     let className = "";
     export { className as class };
+
+    function linkStyle(link) {
+        let styles = [];
+
+        if (!link.url) {
+            styles.push('btn-disabled')
+        }
+
+        styles.push(meta.current_page.toString() == link.label ? activeClass : buttonClass);
+
+        return styles.join(' ');
+    }
 </script>
 
 {#if meta}
     <div class="join {className}" {...$$restProps}>
         {#each meta?.links as link}
             <a
-                href={link.url}
-                class="join-item btn {link.active || !link.url ? 'btn-disabled' : ''} {meta.current_page == link.label ? activeClass : buttonClass}"
+                href={link.url ?? '#'}
+                class="join-item btn {linkStyle(link)}"
                 use:inertia>
                 {@html link.label}
             </a>
