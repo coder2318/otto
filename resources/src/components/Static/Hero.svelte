1051d4e9f76c36c02b293433b72edcd3e60db0e1
eduardbin <binukeduard@gmail.com>
2023-08-08 19:04:35 +0300

    add hero section to landing

---
+++ resources/src/components/Static/Hero.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import bg from '@/assets/img/hero-bg.jpg'
+</script>
+
+<div
+    class="align-items-end hero mb-36 min-h-screen items-end justify-items-start overflow-hidden"
+    style="background-image: url({bg}); border-radius: 0 0 40px 40px;"
+>
+    <div class="hero-overlay bg-black bg-opacity-30" />
+    <div class="hero-content p-28 max-sm:p-16">
+        <div class="max-w-5xl">
+            <h1 class="mb-12 text-7xl text-white max-sm:text-6xl">
+                Every life is a story. Begin telling
+                <span class="italic text-secondary">yours with Otto</span>.
+            </h1>
+
+            <a
+                href="#"
+                class="btn btn-secondary mr-6 h-auto rounded-full px-10 py-4 text-xl font-semibold"
+            >
+                Start Writing
+            </a>
+
+            <a
+                href="#"
+                class="btn btn-secondary h-auto rounded-full bg-white px-10 py-4 text-xl font-semibold"
+            >
+                Free Trial
+            </a>
+        </div>
+    </div>
+</div>
---
+++ resources/src/pages/About.svelte
@@ -0,0 +1,18 @@
+<script context="module" lang="ts">
+    import Base from '@/components/Layouts/Base.svelte'
+    export const layout = [Base]
+</script>
+
+<script lang="ts">
+    import Navbar from '@/components/Navbar.svelte'
+    import Hero from '@/components/Static/Hero.svelte'
+</script>
+
+<svelte:head>
+    <title>{import.meta.env.VITE_APP_NAME} - About</title>
+</svelte:head>
+
+<main class="flex min-h-screen flex-col items-center justify-center">
+    <Navbar />
+    <Hero />
+</main>
