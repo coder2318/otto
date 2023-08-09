bf8db0c25279d6a86b4f620a896aad464615400a
eduardbin <binukeduard@gmail.com>
2023-08-09 21:33:40 +0300

    edit hero responsive

--- resources/src/components/Static/Hero.svelte
+++ resources/src/components/Static/Hero.svelte
@@ -3,27 +3,31 @@
 </script>
 
 <div
-    class="align-items-end hero mb-36 min-h-screen items-end justify-items-start overflow-hidden"
+    class="align-items-end hero mb-36 min-h-screen items-end justify-items-start overflow-hidden max-lg:mb-20 max-md:mb-10"
     style="background-image: url({bg}); border-radius: 0 0 40px 40px;"
 >
     <div class="hero-overlay bg-black bg-opacity-30" />
-    <div class="hero-content p-28 max-sm:p-16">
+    <div
+        class="hero-content p-28 max-lg:p-16 max-sm:p-8 max-[375px]:text-center"
+    >
         <div class="max-w-5xl">
-            <h1 class="mb-12 text-7xl text-white max-sm:text-6xl">
+            <h1
+                class="mb-12 text-7xl text-white max-xl:text-6xl max-lg:text-5xl max-md:mb-8 max-md:text-4xl max-sm:mb-4 max-sm:text-3xl"
+            >
                 Every life is a story. Begin telling
                 <span class="italic text-secondary">yours with Otto</span>.
             </h1>
 
             <a
                 href="#"
-                class="btn btn-secondary mr-6 h-auto rounded-full px-10 py-4 text-xl font-semibold"
+                class="btn btn-secondary mr-6 h-auto rounded-full px-10 py-4 text-xl font-semibold max-lg:mr-5 max-lg:px-9 max-lg:py-3 max-lg:text-lg max-md:mr-4 max-md:px-8 max-md:py-2 max-md:text-base max-[375px]:mb-4 max-[375px]:mr-0"
             >
                 Start Writing
             </a>
 
             <a
                 href="#"
-                class="btn btn-secondary h-auto rounded-full bg-white px-10 py-4 text-xl font-semibold"
+                class="btn btn-secondary h-auto rounded-full bg-white px-10 py-4 text-xl font-semibold max-lg:px-9 max-lg:py-3 max-lg:text-lg max-md:px-8 max-md:py-2 max-md:text-base"
             >
                 Free Trial
             </a>
