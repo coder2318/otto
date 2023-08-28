c821fb9fadb050dd07ea8d58c1cf9ed29efe42be
Dmytro Morozov <dmytro.morozov@lengin.com>
2023-08-28 18:34:56 +0300

    Fix logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,9 @@
 @props(['url'])
 <tr>
     <td class="header">
-        <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('build/assets/logo-row.svg') }}" class="logo" alt="{{ config('app.name') }}" width="250">
+        <a href="{{ $url }}" style="display: flex; align-items: center; justify-content: center; gap: 0.75rem;">
+            <img src="{{ asset('build/assets/logo.png') }}" class="logo" alt="{{ config('app.name') }}" height="50">
+            <span>{{ config('app.name') }}</span>
         </a>
     </td>
 </tr>
