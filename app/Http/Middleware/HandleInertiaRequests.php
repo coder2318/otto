3ff5205a7fd575d0614c2091afeecef95ac2d675
Dmytro Morozov <dmytro.morozov@lengin.com>
2023-09-22 22:26:07 +0300

    Fix user load

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,7 +36,7 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         if (!$request->header('X-Inertia')) {
-            $request->user()->load('unreadNotifications');
+            $request->user()?->load('unreadNotifications');
         }
 
         return array_merge(parent::share($request), [
