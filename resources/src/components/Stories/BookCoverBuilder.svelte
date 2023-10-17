26f35f89b81cb39bd6ab3c685008ee5e5aacf609
Branislav Stojanovic <banestojanovics@gmail.com>
2023-10-17 17:27:50 +0200

    Feature/cover templates (#7)
    * Updated default book cover template to use correct fonts and automatically size and wrap the author and title texts upon user typing
    
    * Updated default book cover template to use correct fonts and automatically size and wrap the author and title texts upon user typing
    
    * Removed .DS_Store files
    
    * Removed .DS_Store files
    
    * Removed .DS_Store files
    
    * Removed .DS_Store files
    
    * Removed .DS_Store files

--- resources/src/components/Stories/BookCoverBuilder.svelte
+++ resources/src/components/Stories/BookCoverBuilder.svelte
@@ -2,15 +2,25 @@
     import { toBlob } from 'html-to-image'
 
     export let template: App.BookCoverTemplate
-    export let parameters: any = {}
+    export let parameters: any = {
+        author: 'Author',
+        title: 'Title',
+        textColor: 'black',
+    }
     export let pages: number = 32
 
     let svg: HTMLElement | SVGElement
 
     $: cover = template.cover ?? import.meta.env.VITE_APP_URL + '/build/assets/cover-background.png'
     $: sizes = getSize(pages)
-    $: updateSvg(parameters)
 
+    $: authorElements = updateTextElements(parameters.author, 'Author')
+    $: titleElements = updateTextElements(parameters.title, 'Title')
+
+    // todo - should be removed if the PR is accepted
+    //$: updateSvg(parameters)
+
+    // todo - should be removed if the PR is accepted
     function updateSvg(parameters) {
         if (!svg) return
 
@@ -65,6 +75,46 @@
     export function getCoverAspectRatio() {
         return (sizes.width + 2 * sizes.safetyMargin) / (sizes.height + 2 * sizes.safetyMargin)
     }
+
+    const fontSize = (stringVar: HTMLElement, startSize: Number) => {
+        if (stringVar?.length > 12) {
+            startSize = 2
+        }
+        if (stringVar?.length > 25) {
+            startSize = 1.5
+        }
+        if (stringVar?.length > 35) {
+            startSize = 1.25
+        }
+
+        return `${startSize}rem`
+    }
+
+    function updateTextElements(text: Array<String>, defaultParam: String) {
+        const words = text?.split(' ') || [defaultParam]
+        let currentLine = ''
+        const newLines = []
+
+        for (let word of words) {
+            if (currentLine.length + word.length + 1 <= 40) {
+                if (currentLine.length > 0) {
+                    currentLine += ' '
+                }
+                currentLine += word
+            } else {
+                newLines.push(currentLine)
+                currentLine = word
+            }
+        }
+
+        if (currentLine.length > 0) {
+            newLines.push(currentLine)
+        } else {
+            newLines.push(defaultParam)
+        }
+
+        return newLines
+    }
 </script>
 
 <svg
@@ -83,24 +133,61 @@
         height={sizes.height + 2 * sizes.safetyMargin}
     />
 
-    <!-- <svg
-        x={sizes.bleedArea}
-        y={sizes.bleedArea}
-        width={sizes.width + 2 * sizes.safetyMargin}
-        height={sizes.height + 2 * sizes.safetyMargin}
-    >
-        {@html template.back ?? ''}
-    </svg>
-    <svg
-        x={sizes.bleedArea}
-        y={sizes.bleedArea}
-        width={sizes.spineWidth}
-        height={sizes.height + 2 * sizes.safetyMargin}
-    >
-        {@html template.spine ?? ''}
-    </svg> -->
+    <style>
+        @import url(https://fonts.googleapis.com/css2?family=Poppins&display=swap);
+    </style>
+    {#each authorElements as text, i}
+        <text
+            x="50%"
+            y={75 + i * 25}
+            font-size={fontSize(parameters?.author, 3)}
+            text-anchor="middle"
+            font-family="Poppins"
+            text-shadow="#FC0 1px 0 10px"
+            data-author="innerText"
+            data-text-color="fill"
+            letter-spacing="0.1em"
+            fill={parameters?.textColor || 'black'}
+            style="text-transform:uppercase"
+        >
+            {text}
+        </text>
+    {/each}
 
-    <svg x={0} y={0} width={sizes.width + 2 * sizes.safetyMargin} height={sizes.height + 2 * sizes.safetyMargin}>
-        {@html template.front ?? ''}
-    </svg>
+    <g transform={`translate(0 -${50 + titleElements?.length * 20})`}>
+        {#each titleElements as text, i}
+            <text
+                x="50%"
+                y={`${100 + i * 3}%`}
+                font-size={fontSize(parameters?.title, 5)}
+                text-anchor="middle"
+                data-title="innerText"
+                data-text-color="fill"
+                text-shadow="#FC0 1px 0 10px"
+                font-family="GhoticRegular"
+                font-weight="bold"
+                letter-spacing="0.1em"
+                fill={parameters?.textColor || 'black'}
+                style="text-transform:uppercase"
+                >{text}
+            </text>
+        {/each}
+    </g>
+
+    <!-- <svg
+            x={sizes.bleedArea}
+            y={sizes.bleedArea}
+            width={sizes.width + 2 * sizes.safetyMargin}
+            height={sizes.height + 2 * sizes.safetyMargin}
+        >
+            {@html template.back ?? ''}
+        </svg>
+        <svg
+            x={sizes.bleedArea}
+            y={sizes.bleedArea}
+            width={sizes.spineWidth}
+            height={sizes.height + 2 * sizes.safetyMargin}
+        >
+            {@html template.spine ?? ''}
+        </svg> -->
 </svg>
