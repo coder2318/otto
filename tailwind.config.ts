3a6fda8fcaddc873bba013110398bd26b59393f1
Dmytro Morozov <dmytro.morozov@lengin.com>
2023-10-17 15:04:08 +0300

    Clear config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,19 +17,6 @@ export default {
             'sans': ['ABC Diatype', ...fontFamily.sans],
             'mono': fontFamily.mono
         },
-        // fontSize: {
-        //     sm: '0.8rem',
-        //     base: '1rem',
-        //     xl: ['18px', '1.6'],
-        //     '2xl': ['20px', '1.6'],
-        //     '3xl': ['24px', '1.1'],
-        //     '4xl': ['28px', '1.1'],
-        //     '5xl': ['32px', '1.1'],
-        //     '6xl': ['48px', '1.1'],
-        //     '7xl': ['54px', '1.1'],
-        //     '8xl': ['60px', '1.1'],
-        //     '9xl': ['86px', '1.2'],
-        // }
     },
     plugins: [
         typography,
