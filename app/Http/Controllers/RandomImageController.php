bddccdaafa0967daf40ea1884ad8ab1d6c8f3aa7
Dmytro Morozov <dmytro.morozov@lengin.com>
2023-12-04 18:41:13 +0200

    Fix random seed

--- app/Http/Controllers/RandomImageController.php
+++ app/Http/Controllers/RandomImageController.php
@@ -10,9 +10,12 @@ class RandomImageController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $seed = $request->query('key');
+        $seed = $seed ? intval($seed) : null;
+
         $media = Media::where('model_type', TimelineQuestion::class)
             ->where('collection_name', 'cover')
-            ->inRandomOrder(intval($request->query('key')))
+            ->inRandomOrder($seed)
             ->firstOrFail();
 
         return redirect($media->getUrl());
