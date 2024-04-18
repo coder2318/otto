26ae9fb00149c027197385af3f8b714a431d9e11
ploskyiyuri <yurii.ploskyi@dotcode.pro>
2024-04-18 17:17:38 +0300

    fix:205 changed covers fields order

--- database/migrations/2024_04_18_153212_add_properties_to_covers_fields-1.php
+++ database/migrations/2024_04_18_153212_add_properties_to_covers_fields-1.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
@@ -12,81 +11,118 @@ return new class extends Migration
     public function up(): void
     {
         $fields = [
-            [
-                'key' => 'descriptionFont',
-                'name' => 'Description Text Font',
-                'type' => 'font',
-                'group' => 'Back',
+            'description' => [
+                [
+                    'key' => 'descriptionFont',
+                    'name' => 'Description Text Font',
+                    'type' => 'font',
+                    'group' => 'Back',
+                ],
+                [
+                    'name' => 'Description Text Size',
+                    'key' => 'descriptionTextSize',
+                    'type' => 'number',
+                    'group' => 'Back',
+                ],
+                [
+                    'name' => 'Description Text Background',
+                    'key' => 'descriptionTextBackground',
+                    'type' => 'color',
+                    'group' => 'Back',
+                ],
             ],
-            [
-                'name' => 'Description Text Background',
-                'key' => 'descriptionTextBackground',
-                'type' => 'color',
-                'group' => 'Back',
+            'subtitle' => [
+                [
+                    'key' => 'subtitleFont',
+                    'name' => 'Subtitle Font',
+                    'type' => 'font',
+                    'group' => 'Front',
+                ],
             ],
-            [
-                'name' => 'Description Text Size',
-                'key' => 'descriptionTextSize',
-                'type' => 'number',
-                'group' => 'Back',
+            'spine' => [
+                [
+                    'key' => 'spineTextFont',
+                    'name' => 'Spine Text Font',
+                    'type' => 'font',
+                    'group' => 'Spine',
+                ],
+                [
+                    'name' => 'Spine Text Size',
+                    'key' => 'spineTextSize',
+                    'type' => 'number',
+                    'group' => 'Spine',
+                ],
+                [
+                    'key' => 'spineBackgroundColor',
+                    'name' => 'Spine Background Color',
+                    'type' => 'color',
+                    'group' => 'Spine',
+                ],
             ],
-            [
-                'key' => 'subtitleFont',
-                'name' => 'Subtitle Font',
-                'type' => 'font',
-                'group' => 'Front',
+            'title' => [
+                [
+                    'key' => 'titleFont',
+                    'name' => 'Title Font',
+                    'type' => 'font',
+                    'group' => 'Front',
+                ],
             ],
-            [
-                'key' => 'spineTextFont',
-                'name' => 'Spine Text Font',
-                'type' => 'font',
-                'group' => 'Spine',
+            'author' => [
+                [
+                    'key' => 'authorFont',
+                    'name' => 'Author Font',
+                    'type' => 'font',
+                    'group' => 'Front',
+                    'after' => 'author',
+                ],
             ],
-            [
-                'key' => 'spineBackgroundColor',
-                'name' => 'Spine Background Color',
-                'type' => 'color',
-                'group' => 'Spine',
-            ],
-            [
-                'key' => 'titleFont',
-                'name' => 'Title Font',
-                'type' => 'font',
-                'group' => 'Front',
-            ],
-            [
-                'key' => 'authorFont',
-                'name' => 'Author Font',
-                'type' => 'font',
-                'group' => 'Front',
-            ],
-            [
-                'name' => 'Spine Text Size',
-                'key' => 'spineTextSize',
-                'type' => 'number',
-                'group' => 'Spine',
-            ]
         ];
 
         $templates = DB::table('book_cover_templates')->get();
+        $this->updateTemplates($templates, $fields);
+    }
+
+    private function updateTemplates($templates, $fields): void
+    {
         foreach ($templates as $template) {
             $decoded = json_decode($template->fields, true);
-            $column = array_column($decoded, 'key');
-            $flag = false;
+            $fields = $this->removeAddedFields($decoded, $fields);
+            $decoded = $this->insertIntoFields($decoded, $fields);
 
-            foreach ($fields as $newField) {
-                if (! in_array($newField['key'], $column)) {
-                    $decoded[] = $newField;
-                    $flag = true;
-                }
-            }
+            DB::table('book_cover_templates')
+                ->where(['id' => $template->id])
+                ->update(['fields' => $decoded]);
+        }
+    }
 
-            if ($flag) {
-                DB::table('book_cover_templates')
-                    ->where(['id' => $template->id])
-                    ->update(['fields' => $decoded]);
+    private function insertIntoFields($decoded, $fields): array
+    {
+        for ($i = 0, $iMax = count($decoded); $i < $iMax; $i++) {
+            if (array_key_exists($decoded[$i]['key'], $fields)) {
+                array_splice($decoded, $i + 1, 0, $fields[$decoded[$i]['key']]);
+                $iMax = count($decoded);
             }
         }
+
+        return $decoded;
+    }
+
+    private function removeAddedFields($decoded, $fields): array
+    {
+        $decoded = array_column($decoded, 'key');
+        $newFields = [];
+        foreach ($fields as $key => $fieldsAfter) {
+            foreach ($fieldsAfter as $fieldToInsert) {
+                if (! in_array($fieldToInsert['key'], $decoded)) {
+                    if (! array_key_exists($key, $newFields)) {
+                        $newFields[$key] = [];
+                    }
+                    $newFields[$key][] = $fieldToInsert;
+                }
+            }
+        }
+
+        return $newFields;
     }
 
     /**
@@ -94,6 +130,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('prompts');
     }
 };
