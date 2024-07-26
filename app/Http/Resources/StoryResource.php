764f052b53a6215d890fdfedc9f3998f4ac78804
BeeJeeNinja <89654075+BeeJeeNinja@users.noreply.github.com>
2024-07-26 18:54:29 +0300

    Revert "#325 fix book cover and Firefox image upload"

--- app/Http/Resources/StoryResource.php
+++ app/Http/Resources/StoryResource.php
@@ -14,43 +14,16 @@ class StoryResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $routeName = $request->route()->getName();
-
-        $cover = $this->whenLoaded('cover', fn () => [
-            'url' => $this->resource->cover?->getUrl(),
-            'meta' => array_merge(
-                $this->resource->cover?->custom_properties,
-                $this->resource->cover?->media
-                    ->mapWithKeys(fn ($media) => [$media->collection_name => $media->getUrl()])
-                    ->all() ?? []
-            ),
-        ]);
-
-        if ($routeName == 'dashboard.stories.cover' || $routeName == 'dashboard.stories.covers') {
-            $cover['meta']['front_base64'] = null;
-            $cover['meta']['back_base64'] = null;
-
-            $medias = $this->resource->cover?->media ?? [];
-            foreach ($medias as $v) {
-                if ($v->collection_name == 'front' || $v->collection_name == 'back') {
-                    if (in_array($v->conversions_disk, ['s3', 's3-public'])) {
-                        $path = $v->getTemporaryUrl(now()->addHour());
-                        $cover['meta'][$v->collection_name] = $path;
-                    } else {
-                        $path = $v->getPath();
-                        $cover['meta'][$v->collection_name] = $v->getUrl();
-                    }
-                    $type = pathinfo($path, PATHINFO_EXTENSION);
-                    $data = file_get_contents($path);
-                    $base64 = 'data:image/'.$type.';base64,'.base64_encode($data);
-
-                    $cover['meta']["{$v->collection_name}_base64"] = $base64;
-                }
-            }
-        }
-
         return array_merge(parent::toArray($request), [
-            'cover' => $cover,
+            'cover' => $this->whenLoaded('cover', fn () => [
+                'url' => $this->resource->cover?->getUrl(),
+                'meta' => array_merge(
+                    $this->resource->cover?->custom_properties,
+                    $this->resource->cover?->media
+                        ->mapWithKeys(fn ($media) => [$media->collection_name => $media->getUrl()])
+                        ->all() ?? []
+                ),
+            ]),
             'book' => $this->whenLoaded('book', fn () => $this->resource->book->getTemporaryUrl(now()->addHour())),
             'book_preview' => $this->whenLoaded('book_preview', fn () => $this->resource->book_preview->getTemporaryUrl(now()->addHour())),
             'book_cover' => $this->whenLoaded('book_cover', fn () => $this->resource->book_cover->getTemporaryUrl(now()->addHour())),
